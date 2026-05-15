a885a2a7ea022ee05bbd4d5e943575c90f041a5c
Aden Alexandria <adenperdana1477@gmail.com>
2026-05-15 18:35:58 +0700

    aden google maps 2

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,24 +1,13 @@
-import { PageHeading } from "../../components/common/page-heading";
 import { PublicNavbar } from "../../components/layout/public-navbar";
 import { SiteFooter } from "../../components/layout/site-footer";
-import { PublicMapPageClient } from "../../components/map/public-map-page-client";
+import { LeafletMapPageClient } from "../../components/maps/leaflet-map-page-client";
 
 export default function MapPage() {
   return (
-    <main className="min-h-screen bg-[#FAFAF7]">
+    <>
       <PublicNavbar />
-
-      <div className="mx-auto max-w-7xl space-y-8 px-6 py-10">
-        <PageHeading
-          eyebrow="Smart Mapping"
-          title="Lihat sebaran masalah kota."
-          description="Peta laporan membantu masyarakat dan pemerintah melihat area yang membutuhkan perhatian lebih cepat."
-        />
-
-        <PublicMapPageClient />
-      </div>
-
+      <LeafletMapPageClient />
       <SiteFooter />
-    </main>
+    </>
   );
 }
